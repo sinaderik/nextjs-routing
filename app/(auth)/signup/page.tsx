054638630bc71ee08065d4955389a5bde2f333f6
Sina <sinaderik7@gmail.com>
2024-10-20 18:05:38 +0330

    login & signup routes are created

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <p className='text-xl'>SignUp</p>
+  )
+}
+
+export default SignUp
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="text-center">
       <hr />
-      home
+      <h2>Home Page</h2>
+      <ul>
+        <li><Link href="/cart">--Cart--</Link></li>
+        <li><Link href="/checkout">--Checkout--</Link></li>
+        <li><Link href="/login">--Login--</Link></li>
+        <li><Link href="/signup">--Register--</Link></li>
+      </ul>
     </div>
   );
 }
